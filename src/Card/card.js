51c8fa9b3292a9e2fe8afa950886369b4d6242af
unknown <net_box@ukr.net>
2022-07-04 13:53:09 +0300

    [START] html css

---
+++ src/Card/card.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "../App.css";
+
+export const Card = ({ city }) =>{
+    console.log('city', city)
+    return ( 
+        <div className="Card">
+            <div className="MainInfo">
+                <img className="Icon" src="https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-02-48.png" alt="icon"/>
+                <div className="Title">{city}</div>
+                <div className="Description">Cloudy</div>
+                <div className="Temperature">20</div>
+            </div>
+            <div className="Information">
+                <div>Humidify: 15</div>
+                <div>Feels leek: 19</div>
+            </div>
+        </div>
+    );
+}
+
+
+
+
+
