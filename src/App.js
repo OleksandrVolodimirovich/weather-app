397d308679573b9e8f27a3a336b1a8205a61aca6
unknown <net_box@ukr.net>
2022-07-04 15:31:26 +0300

    [PROPS] Динамічна зміна назв міст, деструктуризація

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { Input } from './Input/input';
 import { Card } from './Card/card';
 
 function App () {
+  const citiesList = ['New York', 'Kyiv', 'London', 'Lviv'] //* Список назв міст
   return (
     <div className="Main">
           <Input/>
           <div className="CardList">
-            <Card city={'New York'}/>
-            <Card city={'Kyiv'}/>
-            <Card city={'London'}/>
+            {
+              citiesList.map(city => <Card key={city} city={city}/>) //* пребір масива за допомогою map і формування карточки
+                //* react вимагає застосувати ключ key, можна використати змінну, а index краще не використовувати
+            }
           </div>
     </div>
   );
--- src/Card/card.js
+++ src/Card/card.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import "../App.css";
 
-export const Card = ({ city }) =>{
-    console.log('city', city)
+export const Card = ({ city }) =>{    // *застосовую деструктиризацію
     return ( 
         <div className="Card">
             <div className="MainInfo">
                 <img className="Icon" src="https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-02-48.png" alt="icon"/>
-                <div className="Title">{city}</div>
+                <div className="Title">{city}</div> {/* //*Вставляю змінну city */}
+                
                 <div className="Description">Cloudy</div>
                 <div className="Temperature">20</div>
             </div>
