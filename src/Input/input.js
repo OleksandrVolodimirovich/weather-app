ec5de1d095f1a2532f8cc8b90e5d81dc85d04707
unknown <net_box@ukr.net>
2022-07-05 17:07:44 +0300

    [STATE] розділення <input> i <button>

--- src/Input/input.js
+++ src/Input/input.js
@@ -2,23 +2,24 @@ import React, { useState } from "react";
 
 import "../App.css";
 
-export const Input = () => {
-	const [inputValue, setInputValue] = useState('empty') //*[якась змінна, ф-ція котра може змінити значення змінної] = ('тут можна вказати дефолтне значення')
-  const curreDate = new Date().toDateString();
+const InputTag = () => {
+	const [inputValue, setInputValue] = useState('empty')
   const handleOnChange = (event) => {
-    setInputValue(event.target.value); //* за допомогою ф-ції setInputValue змінюю state і засетую в input, котрий рівний тут inputValue
-  };
-
-  const handleOnClick = (value) => () => {
-    console.log("button was clicked in", value);
+    setInputValue(event.target.value);
   };
 
 	console.log('render')
 
   return (
-    <div className="InputWrap">
-      <input className="Input" onChange={handleOnChange} value={inputValue} />  {/*//* задопомогою  value виведу дефолтне значення useState */}
-      <button className="Button" onClick={handleOnClick(curreDate)}>+</button>
-    </div>
+      <input className="Input" onChange={handleOnChange} value={inputValue} />
   );
 };
+
+export const Input = () => (
+	<div className="InputWrap">
+		<InputTag/>
+		<Button/>
+	</div>
+)
+
+const Button = () => (<button className="Button">+</button>)
