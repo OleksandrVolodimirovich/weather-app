9d235729074fd42e108b5c9c0a00053350c4ae72
unknown <net_box@ukr.net>
2022-07-05 13:05:37 +0300

    [On...]onSubmit={handleSubmit} form

--- src/Input/input.js
+++ src/Input/input.js
@@ -3,17 +3,23 @@ import React from "react";
 import "../App.css";
 
 export const Input = () => {
-    const curreDate = new Date().toDateString(); //* об'єкт Date з методом toDateString котри виводить тільки д/м/р
-    const handlOnChange = (event) => { //* ф-ція котра спостерігає за змінами даних в input
-        console.log(event.target.value);
-    }
-    const handlOnClick = (value) => () => { //* Викликаю дві ф-ції в першій curreDate
-        console.log('button was clicked in', value) 
-    }
+  const curreDate = new Date().toDateString();
+  const handleOnChange = (event) => {
+    console.log(event.target.value);
+  };
+  const handleOnClick = (value) => () => {
+    console.log("button was clicked in", value);
+  };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+    console.log("handleSubmit");
+  };
   return (
     <div className="InputWrap">
-      <input className="Input" onChange={handlOnChange} /> {/* //* використання ф-ції handlOnChange для input */}
-      <button className="Button" onClick={handlOnClick(curreDate)}> + </button> {/* //* В onClick помістив змінну curreDate */}
+      <form onSubmit={handleSubmit}>
+        <input className="Input" onChange={handleOnChange} />
+        <button className="Button" onClick={handleOnClick(curreDate)}>+</button>
+      </form>
     </div>
   );
 };
