909b8eac2b702d8ea87009deb14c66dc908d260c
unknown <net_box@ukr.net>
2022-07-05 16:51:15 +0300

    [STATE] useState в input і зміна з handleOnChange

--- src/Input/input.js
+++ src/Input/input.js
@@ -1,25 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../App.css";
 
 export const Input = () => {
+	const [inputValue, setInputValue] = useState('empty') //*[якась змінна, ф-ція котра може змінити значення змінної] = ('тут можна вказати дефолтне значення')
   const curreDate = new Date().toDateString();
   const handleOnChange = (event) => {
-    console.log(event.target.value);
+    setInputValue(event.target.value); //* за допомогою ф-ції setInputValue змінюю state і засетую в input, котрий рівний тут inputValue
   };
+
   const handleOnClick = (value) => () => {
     console.log("button was clicked in", value);
   };
-	const handleSubmit = (event) => {
-		event.preventDefault();
-    console.log("handleSubmit");
-  };
+
+	console.log('render')
+
   return (
     <div className="InputWrap">
-      <form onSubmit={handleSubmit}>
-        <input className="Input" onChange={handleOnChange} />
-        <button className="Button" onClick={handleOnClick(curreDate)}>+</button>
-      </form>
+      <input className="Input" onChange={handleOnChange} value={inputValue} />  {/*//* задопомогою  value виведу дефолтне значення useState */}
+      <button className="Button" onClick={handleOnClick(curreDate)}>+</button>
     </div>
   );
 };
