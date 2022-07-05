28f10c909d29155a6b01c1b5ab6f1d19b49c30c2
unknown <net_box@ukr.net>
2022-07-05 12:53:20 +0300

    [ON...]onClick={handlOnClick(curreDate)} handlOnClick 2-в ф-ції React prac

--- src/Input/input.js
+++ src/Input/input.js
@@ -6,16 +6,14 @@ export const Input = () => {
     const curreDate = new Date().toDateString(); //* об'єкт Date з методом toDateString котри виводить тільки д/м/р
     const handlOnChange = (event) => { //* ф-ція котра спостерігає за змінами даних в input
         console.log(event.target.value);
-        console.log('handlOnChange');
     }
-    const handlOnClick = () => { //* ф-ція кліку по button
-        console.log('handlOnClick');
-        console.log('button was clicked in', curreDate) //* по кліку button викликаємо змінну curreDate з датою
+    const handlOnClick = (value) => () => { //* Викликаю дві ф-ції в першій curreDate
+        console.log('button was clicked in', value) 
     }
   return (
     <div className="InputWrap">
       <input className="Input" onChange={handlOnChange} /> {/* //* використання ф-ції handlOnChange для input */}
-      <button className="Button" onClick={handlOnClick}> + </button> {/* //* використання ф-ції handlOnClick для input */}
+      <button className="Button" onClick={handlOnClick(curreDate)}> + </button> {/* //* В onClick помістив змінну curreDate */}
     </div>
   );
 };
