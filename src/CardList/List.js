56f00a59b78ea51c5d5b23f6203381a41c53ead2
unknown <net_box@ukr.net>
2022-07-13 14:11:39 +0300

    [LES10]add useEffect to List.js

--- src/CardList/List.js
+++ src/CardList/List.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react"; //* додано useEffect
 
 import { Card } from "../Card/card";
 
 import "../App.css";
 
 export const CardList = ({ citiesList }) => {
+	useEffect(() => { 
+		console.log("useEffect => citiesList", citiesList); //* useEffect в консоль
+	});
+	console.log("render"); //* виконання render
 	return (
 		<div className="CardList">
 			{citiesList.map((city) => (
