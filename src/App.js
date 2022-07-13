d1e71d76fb80d1d84c4e8e232f6c9fcdb378a058
unknown <net_box@ukr.net>
2022-07-13 16:54:32 +0300

    [LES10]useEffect unmount

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
 	return (
 		<div className="Main">
 			<Input setCitiesList={setCitiesList} />
-			<CardList citiesList={citiesList} />
-			{/* <div className="CardList">
-            {
-              citiesList.map(city => <Card key={city} city={city}/>) 
-            }
-          </div> */}
+			{citiesList.length === 1 && <CardList citiesList={citiesList} />} 
+      {/* //* Приклад unmount:
+       //* якщо не буде міста - тоді не зарендериться unmount (на екрані не буде карточок)
+       //* якщо одне місто - тоді зарендериться (на екрані буде карточока)
+       //* якщо два міста - тоді не зарендериться unmount (на екрані не буде карточок)*/}
+			{/* <CardList citiesList={citiesList} /> */}
 		</div>
 	);
 }
--- src/CardList/List.js
+++ src/CardList/List.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"; //* додано useEffect
+import React, { useEffect } from "react";
 
 import { Card } from "../Card/card";
 
@@ -6,9 +6,12 @@ import "../App.css";
 
 export const CardList = ({ citiesList }) => {
 	useEffect(() => { 
-		console.log("useEffect => citiesList", citiesList); //* useEffect в консоль
-	}, []); //* другий аргумент useEffect - це []
-	console.log("render"); //* виконання render
+		console.log("useEffect => citiesList", citiesList);
+		return () => { // * function unmount 
+			console.log('unmount');
+		}
+	}, []);
+	console.log("render");
 	return (
 		<div className="CardList">
 			{citiesList.map((city) => (
