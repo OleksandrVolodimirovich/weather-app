583bef689514b3b6e0935940354838cce749d8a4
unknown <net_box@ukr.net>
2022-07-13 13:28:12 +0300

    [LES8]add useRef

--- src/Input/input.js
+++ src/Input/input.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react"; // *добавив hook => useRef
 
 import "../App.css";
 
 export const Input = ({ setCitiesList }) => {
 	const [inputValue, setInputValue] = useState("Lviv");
+  const inputRef = useRef(null); // *помістив у змінну hook => useRef з значенням null
 
 	const handleOnClick = () => {
+    console.log('inputRef =>', inputRef); //* при кліку виводиться hook => useRef, який знаходиться у змінній inputRef
 		setCitiesList((currentArray) => [...currentArray, inputValue]);
 	};
 
@@ -15,7 +17,7 @@ export const Input = ({ setCitiesList }) => {
 
 	return (
 		<div className="InputWrap">
-			<input className="Input" onChange={handleOnChange} value={inputValue} />
+			<input className="Input" onChange={handleOnChange} value={inputValue} ref={inputRef}/> {/* //* прокидуємо в useRef в середину DOM-elementa input */}
 			<button className="Button" onClick={handleOnClick}>
 				+
 			</button>
