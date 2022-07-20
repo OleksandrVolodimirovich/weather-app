2e7b5b5dc1590cbcae350f5a91a750f079a04842
unknown <net_box@ukr.net>
2022-07-20 16:10:28 +0300

    [LES10]багаторазове використання useEffect

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import "./App.css";
 
 import { Input } from "./Input/input";
-// import { Card } from './Card/card';
 import { CardList } from "./CardList/List";
 
 function App() {
@@ -12,10 +11,6 @@ function App() {
 		<div className="Main">
 			<Input setCitiesList={setCitiesList} />
 			{citiesList.length === 1 && <CardList citiesList={citiesList} />} 
-      {/* //* Приклад unmount:
-       //* якщо не буде міста - тоді не зарендериться unmount (на екрані не буде карточок)
-       //* якщо одне місто - тоді зарендериться (на екрані буде карточока)
-       //* якщо два міста - тоді не зарендериться unmount (на екрані не буде карточок)*/}
 			{/* <CardList citiesList={citiesList} /> */}
 		</div>
 	);
--- src/CardList/List.js
+++ src/CardList/List.js
@@ -5,12 +5,20 @@ import { Card } from "../Card/card";
 import "../App.css";
 
 export const CardList = ({ citiesList }) => {
-	useEffect(() => { 
-		console.log("useEffect => citiesList", citiesList);
-		return () => { // * function unmount 
-			console.log('unmount');
-		}
+	useEffect(() => {
+		console.log("mount");
+		const func = () => {
+			console.log("resize");
+		};
+		window.addEventListener('resize', func);
+		return () => {
+			console.log("unmount");
+			window.removeEventListener("resize" , func);
+		};
 	}, []);
+	useEffect(() => {
+		console.log("citiesList lenght" , citiesList.length)
+	}, [citiesList]);
 	console.log("render");
 	return (
 		<div className="CardList">
