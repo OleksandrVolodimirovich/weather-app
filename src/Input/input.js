097d665954711fdef37e6242100a490e60d31f76
unknown <net_box@ukr.net>
2022-07-25 15:54:20 +0300

    [LES12]Припустимо, що зявився новий state someValue.Створю обробник подій onSameValue, який буде сетити збільшуючи його на 1

--- src/Input/input.js
+++ src/Input/input.js
@@ -4,11 +4,12 @@ import "../App.css";
 
 export const Input = ({ setCitiesList }) => {
 	const [inputValue, setInputValue] = useState("Lviv");
+	const [someValue, setSomeValue] = useState("0");
 	const inputRef = useRef(null);
 
 	const handleOnClick = () => {
 		setCitiesList((currentArray) => [...currentArray, inputValue]);
-    setInputValue(''); 
+		setInputValue("");
 		inputRef.current.focus();
 	};
 
@@ -16,15 +17,22 @@ export const Input = ({ setCitiesList }) => {
 		setInputValue(event.target.value);
 	};
 
+	const onSomeClick = (event) => {
+		setSomeValue((someValue) => someValue + 1);
+	};
+	console.log("inputValue => ", inputValue);
+	console.log("from render => ", `${inputValue}_${Math.random()}`);
+	console.log("someValue => ", someValue);
+
 	return (
 		<div className="InputWrap">
 			<input
 				className="Input"
+				onClick={onSomeClick}
 				onChange={handleOnChange}
 				value={inputValue}
 				ref={inputRef}
-			/>{" "}
-			{/* //* прокидуємо в useRef в середину DOM-elementa input */}
+			/>
 			<button className="Button" onClick={handleOnClick}>
 				+
 			</button>
