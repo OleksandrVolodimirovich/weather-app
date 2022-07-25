4a51525db793835b2995dd848b2a0d760758e7e5
unknown <net_box@ukr.net>
2022-07-25 14:47:39 +0300

    [LES11]Виведення даних з API в карточку погоди

--- src/Card/card.js
+++ src/Card/card.js
@@ -4,31 +4,38 @@ import { API_KEY } from "../settings.js";
 import "../App.css";
 
 export const Card = ({ city }) => {
-	const [data, setData] = useState(null)
+	const [data, setData] = useState(null);
 	useEffect(() => {
-		fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
-			.then(res => res.json())
+		fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+		)
+			.then((res) => res.json())
 			// .then(json => setData(json)); - нижче спрощений варіант
 			.then(setData);
 	}, []);
 	console.log("data => ", data);
 
+    // data - видає на першому рендері null, тому неожемо її використовувати. Тобто дані ще не завантажилися, а React пробує щосьвиводити.
+    // Для виправлення ситуації використовується if()
+    // Якщо немає data повернути розмітку з коду "return (div className="Card"></div>)...""
+    if(!data) return null;
+
+	const { name, weather, main } = data;
+	const { description, icon } = weather[0];
+	const { temp, humidity, feels_like } = main;
+
 	return (
 		<div className="Card">
 			<div className="MainInfo">
-				<img
-					className="Icon"
-					src="https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-02-48.png"
-					alt="icon"
-				/>
-				<div className="Title">{city}</div>
+				<img className="Icon" src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="icon"/>
+				<div className="Title">{name}</div> {/*Назва міста */}
 
-				<div className="Description">Cloudy</div>
-				<div className="Temperature">20</div>
+				<div className="Description">{description}</div> {/* Опис погодм */}
+				<div className="Temperature">{temp.toFixed()}</div> {/* Температура */}
 			</div>
 			<div className="Information">
-				<div>Humidify: 15</div>
-				<div>Feels leek: 19</div>
+				<div>Humidify: {humidity}</div> {/* Вологість */}
+				<div>Feels leek: {feels_like}</div> {/* Відчувається, як */}
 			</div>
 		</div>
 	);
